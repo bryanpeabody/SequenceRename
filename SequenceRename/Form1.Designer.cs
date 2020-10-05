da148a272b5e049ac63a0e7a42e1b7a317eb6f3e
Bryan Peabody <hi@bryanpeabody.com>
2020-10-05 12:58:45 -0400

    Initial add.

---
+++ SequenceRename/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SequenceRename
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbSourceDir = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source";
+            // 
+            // tbSourceDir
+            // 
+            this.tbSourceDir.Location = new System.Drawing.Point(86, 25);
+            this.tbSourceDir.Name = "tbSourceDir";
+            this.tbSourceDir.Size = new System.Drawing.Size(578, 23);
+            this.tbSourceDir.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(681, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnRename
+            // 
+            this.btnRename.Location = new System.Drawing.Point(681, 53);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(75, 23);
+            this.btnRename.TabIndex = 5;
+            this.btnRename.Text = "Rename";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 96);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbSourceDir);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Sequence Rename";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbSourceDir;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRename;
+    }
+}
+
---
+++ SequenceRename/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SequenceRename
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dir = folderBrowserDialog1.ShowDialog();
+            tbSourceDir.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            var inputPath = folderBrowserDialog1.SelectedPath;
+            var outputPath = folderBrowserDialog1.SelectedPath + @"\Renamed";
+            
+            // Create output dir
+            Directory.CreateDirectory(outputPath);
+
+            // Input dir and files
+            DirectoryInfo dir = new DirectoryInfo(inputPath);
+
+            // Get files, oldest to newest
+            FileInfo[] files = dir.GetFiles("*.jpg").OrderBy(f => f.CreationTimeUtc).ToArray();
+
+            int count = 1;
+            string newFileName = string.Empty;
+
+            foreach (var file in files)
+            {                    
+                if (count < 10)
+                {
+                    newFileName = "000" + count;
+                }
+                else if (count >= 10 && count < 100)
+                {
+                    newFileName = "00" + count;
+                }
+                else if (count >= 100 && count < 1000)
+                {
+                    newFileName = "0" + count;
+                }
+                else
+                {
+                    newFileName = count.ToString();
+                }
+
+                file.CopyTo(outputPath + @"\" + newFileName + ".jpg");
+
+                count++;
+            }
+        }
+    }
+}
